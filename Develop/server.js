fd696f750b971e2e79f36734e920cbd9dcfc58ed
FranklinBrad <franklin1jbradley@gmail.com>
2023-11-02 20:17:08 -0500

    added readfile

---
+++ Develop/server.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const path = require('path');
+const fs = require('fs')
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use(express.static('public'));
+
+app.get('/api/notes', (req, res) => {
+fs.readFile('db/db.json', 'utf8', (error, data) =>{
+if(error) {
+   console.log('error')
+ }else { 
+  console.log(typeof data)
+  res.send(data)
+}
+});
+
+});
+
+
+app.get('/api/notes', (req, res) => {
+  fs.appendFile('./db.json', 'utf8', (err) =>
+    err ? console.error(err) : console.log('Success!')
+)
+});
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/Develop/public/index.html'))
+);
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public//notes.html'))
+);
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT}`)
+);
